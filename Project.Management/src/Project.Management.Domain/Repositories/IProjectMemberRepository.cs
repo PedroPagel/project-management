ddad6c324a1ee929401d54cf17b5e62faef04587
PedroPagel <pedro.pagel@live.com>
2025-08-25 13:53:12 +0100

    ProjectMembers

--- Project.Management/src/Project.Management.Domain/Repositories/IProjectMemberRepository.cs
+++ Project.Management/src/Project.Management.Domain/Repositories/IProjectMemberRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Project.Management.Domain.Entities;
+using Project.Management.Domain.Services.Projects.Models;
 
 namespace Project.Management.Domain.Repositories
 {
     public interface IProjectMemberRepository : IRepository<ProjectMember>
     {
-        public Task<bool> Delete(Guid userId, Guid id);
+        Task<bool> Delete(Guid userId, Guid id);
+        Task<ProjectMemberQueryValidation> GetProjectMemberLink(ProjectMemberCreationRequest projectmemberCreationRequest);
     }
 }
---
+++ Project.Management/src/Project.Management.Domain/Services/Projects/Models/ProjectMemberRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Project.Management.Domain.Services.Projects.Models
+{
+    public class ProjectMemberRequest
+    {
+        public Guid ProjectMemberId { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid RoleId { get; set; }
+    }
+}
