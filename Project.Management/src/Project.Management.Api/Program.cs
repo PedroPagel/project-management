62049733dda438d88d6239430c706138a00e84b2
Pedro Leonardo Pagel <pedro.pagel@live.com>
2025-08-25 14:31:28 +0100

    Remover sonar errors

--- Project.Management/src/Project.Management.Api/Program.cs
+++ Project.Management/src/Project.Management.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Project.Management.Api.Mappings;
 using Project.Management.Api.Middlewares;
+using Project.Management.Aspire.ServiceDefaults;
 using Project.Management.Infrastructure.Configurations;
 using Project.Management.Infrastructure.Data;
 using Project.Management.Infrastructure.Extensions;
