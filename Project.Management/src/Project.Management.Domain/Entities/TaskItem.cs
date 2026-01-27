2ba163d72400e4a729481ee1da2c09e1c6164957
Pedro Leonardo Pagel <pedro.pagel@live.com>
2026-01-27 10:31:42 +0000

    Save taks issues
    Save taks issues

--- Project.Management/src/Project.Management.Domain/Entities/TaskItem.cs
+++ Project.Management/src/Project.Management.Domain/Entities/TaskItem.cs
@@ -7,7 +7,7 @@
 
         public string Title { get; set; }
         public string Description { get; set; }
-        public Enums.TaskStatus Status { get; set; }
+        public Enums.TaskState Status { get; set; }
 
         public DateTime CreatedAt { get; set; }
         public DateTime? DueDate { get; set; }
