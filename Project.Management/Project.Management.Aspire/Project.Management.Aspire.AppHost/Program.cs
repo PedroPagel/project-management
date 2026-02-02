d347533a910aab9058e4e71323b9afee28a37fde
PedroPagel <pedro.pagel@live.com>
2026-02-02 23:24:14 +0000

    Add hangfire worker and expand seed data

--- Project.Management/Project.Management.Aspire/Project.Management.Aspire.AppHost/Program.cs
+++ Project.Management/Project.Management.Aspire/Project.Management.Aspire.AppHost/Program.cs
@@ -3,5 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.Project_Management_Api>("apiservice");
+var hangfireService = builder.AddProject<Projects.Project_Management_Hangfire>("hangfire");
 
 await builder.Build().RunAsync();
